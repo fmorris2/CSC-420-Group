7cd39706956e65ae29e1f3108b8ccb295c7f2521
Freddy Jr <Freddy Jr>
2016-10-24 21:09:45 -0400

    Added example info for the JTree

--- src/InfoList.java
+++ src/InfoList.java
@@ -3,6 +3,7 @@ import java.awt.Color;
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 import javax.swing.JTree;
+import javax.swing.tree.DefaultMutableTreeNode;
 
 import net.miginfocom.swing.MigLayout;
 
@@ -14,11 +15,40 @@ public class InfoList extends JPanel
 	
 	public InfoList()
 	{
-		tree = new JTree();
+	
+		/*
+		 * THIS IS TREE INFO JUST FOR AN EXAMPLE FOR THE PRESENTATION
+		 * HORRIBLY BRUTE FORCED WAY TO DO IT, WILL BE COMPLETELY DIFFERENT
+		 * WHEN THE REAL SYSTEM IS IMPLEMENTED
+		 */
+        DefaultMutableTreeNode root = new DefaultMutableTreeNode("Classes");
+        DefaultMutableTreeNode mathNode = new DefaultMutableTreeNode("Math");
+        DefaultMutableTreeNode studentNode = new DefaultMutableTreeNode("Students");
+        DefaultMutableTreeNode currentAssNode = new DefaultMutableTreeNode("Current Assignments");
+        DefaultMutableTreeNode pastDueAssNode = new DefaultMutableTreeNode("Past Due Assignments");
+        DefaultMutableTreeNode studentNode1 = new DefaultMutableTreeNode("Students");
+        DefaultMutableTreeNode currentAssNode1 = new DefaultMutableTreeNode("Current Assignments");
+        DefaultMutableTreeNode pastDueAssNode1 = new DefaultMutableTreeNode("Past Due Assignments");
+        DefaultMutableTreeNode ssNode = new DefaultMutableTreeNode("Social Studies");
+		
+        mathNode.add(studentNode);
+        mathNode.add(currentAssNode);
+        mathNode.add(pastDueAssNode);
+        
+        ssNode.add(studentNode1);
+        ssNode.add(currentAssNode1);
+        ssNode.add(pastDueAssNode1);
+        
+        root.add(mathNode);
+        root.add(ssNode);
+		
+        //END EXAMPLE TREE
+		
+		tree = new JTree(root);
 		setLayout(new MigLayout("fill", "10[grow, fill]", ""));
 		setBorder(BorderFactory.createLineBorder(Color.GRAY, 1));
 		
-		add(tree, "grow, width max(150, 50%)");
+		add(tree, "grow, width max(200, 50%)");
 	}
 
 }
